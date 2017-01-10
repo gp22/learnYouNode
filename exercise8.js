8aa063d1ec104fcbd1e2be3480ff68c28bec3122
gp22 <gp22@tutanota.com>
2017-01-10 15:41:58 -0800

    reworked exercise8 and solved exercise9

--- exercise8.js
+++ exercise8.js
@@ -1,18 +1,26 @@
+var bl = require('bl');
 var http = require('http');
 var url = process.argv[2];
 
+// http.get(url, function(res) {
+//     var string = '';
+//
+//     res.setEncoding('utf8');
+//     res.on('data', function(data) {
+//       string += data;
+//     });
+//
+//     res.on('end', function() {
+//       console.log(string.length);
+//       console.log(string);
+//     });
+//
+//     res.on('error', console.error);
+// }).on('error', console.error);
+
 http.get(url, function(res) {
-    var string = '';
-
-    res.setEncoding('utf8');
-    res.on('data', function(data) {
-      string += data;
-    });
-
-    res.on('end', function() {
-      console.log(string.length);
-      console.log(string);
-    });
-
-    res.on('error', console.error);
-}).on('error', console.error);
+    res.pipe(bl(function(err, data) {
+      console.log(data.toString().length);
+      console.log(data.toString());
+    }));
+});
--- exercise9.js
+++ exercise9.js
@@ -1,52 +1,67 @@
+var bl = require('bl');
 var http = require('http');
 var url1 = process.argv[2];
 var url2 = process.argv[3];
 var url3 = process.argv[4];
 
-var getData = function(url, callback) {
-    http.get(url, function(err, res) {
-        if (err) {
-            return callback(err)
-        }
+var getData1 = function(callback) {
+    http.get(url1, function(res) {
 
         callback(null, res)
     })
 }
 
-getData(url1, function(err, res) {
+var getData2 = function(callback) {
+    http.get(url2, function(res) {
+
+        callback(null, res)
+    })
+}
+
+var getData3 = function(callback) {
+    http.get(url3, function(res) {
+
+        callback(null, res)
+    })
+}
+
+getData1(function(err, res) {
     if (err) {
         return console.error(err)
     }
-
-    var string = '';
-
-    res.setEncoding('utf8');
-    res.on('data', function(data) {
-        string += data;
-    })
-
-    res.on('end', function() {
-        console.log(string);
-    })
-    res.on('error', console.error)
+    res.pipe(bl(function(err, data) {
+        if (err) {
+            return console.error(err)
+        }
+        var data = data.toString();
+        console.log(data);
+    }));
+    // begin data2
+    getData2(function(err, res) {
+        if (err) {
+            return console.error(err)
+        }
+        res.pipe(bl(function(err, data) {
+            if (err) {
+                return console.error(err)
+            }
+            var data = data.toString();
+            console.log(data);
+        }));
+        // begin data3
+        getData3(function(err, res) {
+            if (err) {
+                return console.error(err)
+            }
+            res.pipe(bl(function(err, data) {
+                if (err) {
+                    return console.error(err)
+                }
+                var data = data.toString();
+                console.log(data);
+            }));
+        });
+        // end data3
+    });
+    // end data2
 });
-
-
-
-
-
-
-
-// var printData = function(url, callback) {
-//     fs.readdir(dir, function(err, list) {
-//         if (err) {
-//             return callback(err)
-//         }
-//
-//         list = list.filter(function(file) {
-//             return path.extname(file) === '.' + filterStr
-//         })
-//
-//         callback(null, res)
-//     })
-// }
